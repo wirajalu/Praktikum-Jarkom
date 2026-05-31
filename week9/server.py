3381fe355518f3081c68fd7b71105109e4b2fe4c
Wirajalu Setyonegoro Wibowo <wirajalu01@gmail.com>
2026-05-31 23:55:37 +0700

    laporan7

---
+++ week9/server.py
@@ -0,0 +1,29 @@
+from socket import *
+import sys
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverPort = 6789
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+
+while True:
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+        
+        # Header HTTP
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+        
+        # Mengirim isi file
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+    except IOError:
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+        connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>\r\n".encode())
+        connectionSocket.close()
